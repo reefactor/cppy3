e97e21476c4114264c0f9fad5d14086be9e6e6d3
Den <dennis.victorovich@gmail.com>
2023-12-05 19:22:00 +0000

    fix DLL exports naming

--- cppy3/libdefs.hpp
+++ cppy3/libdefs.hpp
@@ -3,13 +3,13 @@
 #if (defined(_WIN32) || defined(__CYGWIN__))
     #pragma warning(disable : 4996 4244 4180 4800 4661)
     // if we are building the dynamic library (instead of using it)
-    #if defined(cppy2_EXPORTS)
+    #if defined(cppy3_EXPORTS)
         #define LIB_API __declspec(dllexport)
     #else
         #define LIB_API __declspec(dllimport)
     #endif
 #else
-    #if defined(cppy2_EXPORTS)
+    #if defined(cppy3_EXPORTS)
         #define LIB_API __attribute__((visibility("default")))
     #else
         #define LIB_API

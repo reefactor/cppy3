55d8d4f2a108a1d94908c94a6344b3344a81c2c8
Paolo Bosetti <paolo.bosetti@unitn.it>
2024-09-02 09:26:25 +0200

    Since we're only building static libs, __declspec is disabled on Windows

--- cppy3/libdefs.hpp
+++ cppy3/libdefs.hpp
@@ -3,11 +3,12 @@
 #if (defined(_WIN32) || defined(__CYGWIN__))
     #pragma warning(disable : 4996 4244 4180 4800 4661)
     // if we are building the dynamic library (instead of using it)
-    #if defined(cppy3_EXPORTS)
-        #define LIB_API __declspec(dllexport)
-    #else
-        #define LIB_API __declspec(dllimport)
-    #endif
+    // #if defined(cppy3_EXPORTS)
+    //     #define LIB_API __declspec(dllexport)
+    // #else
+    //     #define LIB_API __declspec(dllimport)
+    // #endif
+    #define LIB_API 
 #else
     #if defined(cppy3_EXPORTS)
         #define LIB_API __attribute__((visibility("default")))
